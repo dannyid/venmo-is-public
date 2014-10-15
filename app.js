184fcc411354e760ff6aa9f9229e30c29ccb6291
dannyid <danielshekhtman@gmail.com>
2014-10-15 12:00:53 -0400

    adding transaction permalink and Facebook profile URLs

--- app.js
+++ app.js
@@ -7,21 +7,34 @@ var request = require("request"),
 
 request('https://venmo.com/api/v5/public', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    var dataArray = JSON.parse(body).data;
-    var dataLength = dataArray.length;
-    var c;
-    console.log(dataArray);
-    
+    var dataArray = JSON.parse(body).data,
+        dataLength = dataArray.length,
+        c,
+        actorFacebookUrl,
+        targetFacebookUrl;
+
+    function getFacebookUrl(pictureUrl) {
+      if (pictureUrl.indexOf("facebook") >= 0) {
+        return "(https://www.facebook.com/"+pictureUrl.split("/")[3]+")";
+      } else {
+        return ""
+      };
+    };
+
     console.log("#####################################################\n#####################################################".white.bold)
     for(var i = 0; i < dataLength; i = i + 1){
       c = dataArray[i]
-      
+
+      actorFacebookUrl = getFacebookUrl(c.actor.picture);
+      targetFacebookUrl = getFacebookUrl(c.transactions[0].target.picture);
+
       console.log("Payment ID: "+c.payment_id+"\n"+
                   "Story ID:   "+c.story_id+"\n"+
+                  "Permalink:  https://www.venmo.com"+c.permalink+"\n"+
                   "From ID:    "+c.actor.id.red+"\n"+
-                  "From:       "+c.actor.username.red+"\n"+
+                  "From:       "+c.actor.username.red.bold+" "+actorFacebookUrl+"\n"+
                   "To ID:      "+c.transactions[0].target.id.magenta+"\n"+
-                  "To:         "+c.transactions[0].target.username.magenta+"\n"+
+                  "To:         "+c.transactions[0].target.username.magenta.bold+"\n"+
                   "Via:        "+c.via+"\n"+
                   "Type:       "+c.type+"\n"+
                   "Message:    "+c.message.green.bold+"\n");
